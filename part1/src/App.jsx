55044c4e64808f96c682cadb0f1239c2f4838dce
Eugene Gincu <eugene.gincu@gmail.com>
2026-03-08 15:59:21 -0600

    part1 a: React multiple components, props

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const date = new Date();
 
-function App() {
-  const [count, setCount] = useState(0)
+const Hello = (props) => {
+    console.log(props);
+    return (
+        //To avoid having extra div elements use fragments
+        <>
+            <p>Hello {props.name}, you are {props.age} years old</p>
+        </>
+    );
+}
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const App = () => {
+    const name = 'Peter';
+    const age = 10;
+
+    return (
+        <div>
+            <h1>Greetings</h1>
+            <Hello />
+            <Hello name='Maya' age={26 + 10} />
+            <Hello name={name} age={age} />
+            <p>{date.toString()}</p>
+        </div>
+    );
 }
 
 export default App
