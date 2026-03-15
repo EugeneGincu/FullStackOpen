fc849780705737df3ed7eaa293d8a153b1ef5859
Eugene Gincu <eugene.gincu@gmail.com>
2026-03-15 17:32:25 -0600

    part1c passting state to child components

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,24 +1,32 @@
 import {useState} from 'react'
 
+const Display = ({counter}) => <div>{counter}</div>
+
+// OnSomething is recommended by react for props which take event handler functions
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+
 const App = () => {
     const [ counter, setCounter] = useState(0)
 
+    // handleSomething is recommended by react for event handler functions
     const handleClick = () => {
         console.log('clicked')
     }
 
+    const increaseByOne = () => setCounter(counter + 1)
+    const decreaseByOne = () => setCounter(counter - 1)
+    const setToZero = () => setCounter(0)
+
 
     return (
         <div>
-            <div>{counter}</div>
-            <button onClick={handleClick}>
+            <Display counter={counter}/>
+            <Button onClick={increaseByOne} text='plus'/>
+            <Button onClick={setToZero} text='zero' />
+            <Button onClick={decreaseByOne} text='minus' />
 
-                plus
-            </button>
-            <button onClick={() => setCounter(counter + 1)}>
-                counter
-            </button>
             <button onClick={() => setCounter(0)}>
+                {/*Not a good idea to define event handlers within JSX-templates*/}
                 zero
             </button>
         </div>
