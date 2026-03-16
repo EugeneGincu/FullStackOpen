06a1dc004c17ff806e464de3406127fa9cabce18
Eugene Gincu <eugene.gincu@gmail.com>
2026-03-16 11:21:25 -0600

    part1d multiple states

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,36 +1,21 @@
 import {useState} from 'react'
 
-const Display = ({counter}) => <div>{counter}</div>
-
-// OnSomething is recommended by react for props which take event handler functions
-const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
-
 const App = () => {
-    const [ counter, setCounter] = useState(0)
-
-    // handleSomething is recommended by react for event handler functions
-    const handleClick = () => {
-        console.log('clicked')
-    }
-
-    const increaseByOne = () => setCounter(counter + 1)
-    const decreaseByOne = () => setCounter(counter - 1)
-    const setToZero = () => setCounter(0)
-
+    const [left, setLeft] = useState(0)
+    const [right, setRight] = useState(0)
 
     return (
         <div>
-            <Display counter={counter}/>
-            <Button onClick={increaseByOne} text='plus'/>
-            <Button onClick={setToZero} text='zero' />
-            <Button onClick={decreaseByOne} text='minus' />
-
-            <button onClick={() => setCounter(0)}>
-                {/*Not a good idea to define event handlers within JSX-templates*/}
-                zero
+            {left}
+            <button onClick={() => setLeft(left + 1)}>
+                left
             </button>
+            <button onClick={() => setRight(right + 1)}>
+                right
+            </button>
+            {right}
         </div>
-    );
+    )
 }
 
 export default App
