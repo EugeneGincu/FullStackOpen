d7befc966f06ea78a6197a11af32ea954f72568f
Eugene Gincu <eugene.gincu@gmail.com>
2026-03-17 09:12:10 -0600

    part1d asynchronous state update issue

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
         setAll(allClicks.concat('L'))
         const updatedLeft = left + 1 // Solution to display correct state
         setLeft(updatedLeft)
-        setTotal(updatedLeft + right)
+        setTotal(updatedLeft + right) // Solution to display correct state
     }
 
     const handleRightClick = () => {
