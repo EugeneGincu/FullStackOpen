7edaa7c5b7af6963d45011705b33c527a0016936
Eugene Gincu <eugene.gincu@gmail.com>
2026-04-06 17:15:08 -0600

    part1d functions within functions

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,49 +1,33 @@
 import {useState} from 'react'
 
-const History = (props) => {
-    if (props.allClicks.length === 0) {
-        return (
-            <div>
-                the app is used by pressing the buttons
-            </div>
-        )
-    }
-    return (
-        <div>
-            button press history: {props.allClicks.join(' ')}
-        </div>
-    )
-}
-
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
-
 const App = () => {
-    const [left, setLeft] = useState(0)
-    const [right, setRight] = useState(0)
-    const [allClicks, setAll] = useState([])
-    const [total, setTotal] = useState(0)
+    const  [value, setValue] = useState(10);
 
-    const handleLeftClick = () => {
-        setAll(allClicks.concat('L'))
-        const updatedLeft = left + 1
-        setLeft(updatedLeft)
-        setTotal(updatedLeft + right)
+const Button = (props) => (
+    <button onClick={props.onClick}>
+        {props.text}
+    </button>
+)
+
+//Either way is fine
+    const setToValueRetFunc = (newValue) => () => {
+        console.log('value now', newValue);
+        setValue(newValue);
     }
 
-    const handleRightClick = () => {
-        setAll(allClicks.concat('R'))
-        const updatedRight = right + 1
-        setRight(updatedRight)
-        setTotal(left + updatedRight)
+    const setToValueCallFunc = (newValue) => {
+        console.log('value now', newValue);
+        setValue(newValue);
     }
+
 
     return (
         <div>
-            {left}
-            <Button onClick={handleLeftClick} text='left' />
-            <Button onClick={handleRightClick} text='right' />
-            {right}
-            <History allClicks={allClicks} />
+            {value}
+            <button onClick={setToValueRetFunc(1000)}>thousand</button>
+            <button onClick={() => setToValueCallFunc(0)}>reset</button>
+            <button onClick={setToValueRetFunc(value + 1)}>increment</button>
+            <Button onClick={() => setToValueCallFunc(value + 1)} text="increment1" />
         </div>
     )
 }
