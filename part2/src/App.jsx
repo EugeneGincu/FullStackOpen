beb627c19cd2f57a420dd05339b994222ec9bcfc
Eugene Gincu <eugene.gincu@gmail.com>
2026-04-10 10:33:11 -0600

    key attribute

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,15 +1,26 @@
 import { useState } from 'react'
 
-const App = (props) => {
-    const {notes} = props
+const Note = ({note}) => {
+    return (
+        <li>{note.content}</li>
+    )
+}
+
+const App = ({notes}) => {
+    // Instead of const {notes} = props
 
     return (
         <div>
             <h1>Notes</h1>
             <ul>
-                <li>{notes[0].content}</li>
-                <li>{notes[1].content}</li>
-                <li>{notes[2].content}</li>
+                {notes.map(note =>
+                    <Note key={note.id} note={note} /> // key attribute is now defined for the Note component, not li tag
+                    /* Instead of
+                    <li key={note.id}>
+                        {note.content}
+                    </li>
+                    */
+                )}
             </ul>
         </div>
     )
